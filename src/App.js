ed3d085188694512352cc1c98cd2215dc77bd646
Mazzy45 <nurmaziah45@gmail.com>
2025-01-06 01:56:51 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DetailsPage from './components/DetailsPage';
 
 function App() {
     return (
-        <Router> {/* Removed basename */}
+        <Router basename="/touristspots">
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/touristspots/:id" element={<DetailsPage />} />
